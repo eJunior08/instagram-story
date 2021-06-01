df4688d08b24521f5cfb7c7774304fd7b9aa7be5
Elson Junior <elsonfojr@gmail.com>
2021-06-01 19:03:47 -0300

    Novos dados inseridos

--- src/data/stories2.js
+++ src/data/stories2.js
@@ -33,6 +33,95 @@ const stories2 = [
     avatar: require('../assets/avatars/jmitch.png'),
     user: 'jmitch',
   },
+  {
+    id: '3',
+    photos: [
+      {
+        source:
+          'https://images.pexels.com/photos/6647919/pexels-photo-6647919.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      },
+      {
+        source:
+          'https://images.pexels.com/photos/4493795/pexels-photo-4493795.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      },
+      {
+        source:
+          'https://images.pexels.com/photos/1232594/pexels-photo-1232594.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      },
+      {
+        source:
+          'https://images.pexels.com/photos/2444429/pexels-photo-2444429.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      },
+    ],
+    avatar: require('../assets/avatars/jmitch.png'),
+    user: 'jmitch',
+  },
+  {
+    id: '4',
+    photos: [
+      {
+        source:
+          'https://images.pexels.com/photos/2832039/pexels-photo-2832039.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      },
+      {
+        source:
+          'https://images.pexels.com/photos/887849/pexels-photo-887849.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      },
+      {
+        source:
+          'https://images.pexels.com/photos/2168974/pexels-photo-2168974.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      },
+      {
+        source:
+          'https://images.pexels.com/photos/1314186/pexels-photo-1314186.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      },
+      {
+        source:
+          'https://images.pexels.com/photos/670786/pexels-photo-670786.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      },
+      {
+        source:
+          'https://images.pexels.com/photos/604969/pexels-photo-604969.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      },
+      {
+        source:
+          'https://images.pexels.com/photos/2940256/pexels-photo-2940256.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      },
+      {
+        source:
+          'https://images.pexels.com/photos/3659862/pexels-photo-3659862.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      },
+      {
+        source:
+          'https://images.pexels.com/photos/3789885/pexels-photo-3789885.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      },
+      {
+        source:
+          'https://images.pexels.com/photos/4057697/pexels-photo-4057697.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      },
+    ],
+    avatar: require('../assets/avatars/jmitch.png'),
+    user: 'jmitch',
+  },
+  {
+    id: '5',
+    photos: [
+      {
+        source:
+          'https://images.pexels.com/photos/6479560/pexels-photo-6479560.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      },
+      {
+        source:
+          'https://images.pexels.com/photos/1928645/pexels-photo-1928645.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+      },
+      {
+        source:
+          'https://images.pexels.com/photos/6287554/pexels-photo-6287554.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+      },
+    ],
+    avatar: require('../assets/avatars/jmitch.png'),
+    user: 'jmitch',
+  },
 ];
 
 export default stories2;
